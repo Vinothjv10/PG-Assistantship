865f075957146c037e1b541fe87df42036ec2d5c
Vinothjv10 <vinothjv10@gmail.com>
2022-02-13 23:09:11 +0530

    validation over🎶🥳

--- src/app/details-form/details-form.component.ts
+++ src/app/details-form/details-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-details-form',
@@ -7,11 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsFormComponent implements OnInit {
 
-  constructor() { }
+  registerForm: any = FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
-  }
-  submit() {
+    this.registerForm = this.formBuilder.group({
+      year: ['', Validators.required],
+      courseName: ['', Validators.required],
+      branch: ['', Validators.required],
+      Name: ['', Validators.required],
+      communa_lCategory: ['', Validators.required],
+      community_Admitted: ['', Validators.required],
+      entry_Mode: ['', Validators.required],
+      tancet: ['', Validators.required],
+      gate: ['', Validators.required],
+      history_Arrear: ['', Validators.required],
+      attendance: ['', Validators.required],
+      account_No: ['', Validators.required],
+      bank_Name: ['', Validators.required],
+      ifsc_code: ['', Validators.required],
+
+    });
 
   }
+  get f() { return this.registerForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    // display form values on success
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
+  }
+
+  onReset() {
+    this.submitted = false;
+    this.registerForm.reset();
+  }
+
 }
